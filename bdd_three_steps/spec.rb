cc77f5be5a9fdc0133731c2b1e95b2bbeb23c46a
Bryan Liff <bliff@minerva-group.com>
2022-06-30 19:30:22 -0400

    BDD-42 Spec implementation: User object sends UserReportMailer

--- bdd_three_steps/spec.rb
+++ bdd_three_steps/spec.rb
@@ -1,11 +1,15 @@
 # spec/models/user_spec.rb
 
 describe User do
-    describe 'methods' do
-        describe 'instance' do
-            describe '#send_mailer_user_report' do
-                it 'calls UserReportMailer.new(self).send()'
-            end
+  describe 'methods' do
+    describe 'instance' do
+      describe '#send_mailer_user_report' do
+        it 'sends a UserReportMailer for itself' do
+          expect(any_instance_of(UserReportMailer)).to_receive(:send).once
+          user = FactoryBot.build(:user)
+          user.send_mailer_user_report
         end
+      end
     end
+  end
 end
